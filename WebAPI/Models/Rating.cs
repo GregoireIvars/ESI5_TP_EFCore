4c2d20661f91e3b3aa9086f0760b0e59a75daae2
gregoire.ivars <gregoire.ivars@inetum.com>
2025-04-08 13:52:34 +0200

    feat: Initialisation de la BDD avec Début de contrainte

---
+++ WebAPI/Models/Rating.cs
@@ -0,0 +1,15 @@
+namespace WebApi.Data
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int SessionId { get; set; }
+        public int ParticipantId { get; set; }
+        public int Score { get; set; }
+        public required string Comments { get; set; }
+
+        public required Session Session { get; set; }
+        public required Participant Participant { get; set; }
+    }
+
+}
---
+++ WebAPI/Models/Speaker.cs
@@ -0,0 +1,15 @@
+namespace WebApi.Data
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Bio { get; set; }
+        public required string Email { get; set; }
+        public required string Company { get; set; }
+
+        public required ICollection<SessionSpeaker> SessionSpeakers { get; set; }
+    }
+
+}
